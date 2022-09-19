4d26518df63876d9e9c2b48e1b7b299a93afdca1
gkrl89 <karpagarajalakshmi.g@10decoders.in>
2022-09-19 13:18:28 +0530

    all commits

--- src/CRUD/submenu.tsx
+++ src/CRUD/submenu.tsx
@@ -1,79 +1,80 @@
 import React, { useEffect, useState } from "react";
-import { Link , useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { SidebarData } from "./sidebardata";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const SidebarLink = styled(Link)`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 3.75rem;
-    font-size: 15px;
-    padding: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 3.75rem;
+  font-size: 15px;
+  padding: 2rem;
+  text-decoration: none;
+  color: #ffffff;
+  border: 0.1px solid #ddd;
+  &:hover {
+    background-color: #1f1f1b;
+    border-left: 4px solid #9c27b0;
+    color: #fff;
     text-decoration: none;
-    color: #ffffff;
-    border: 0.1px solid #ddd;
-    &:hover {
-        background-color: #1f1f1b;
-        border-left: 4px solid #9c27b0;
-        color:#fff;
-        text-decoration: none;
-    }
-    &:visited {
-        color:#fff;
-        text-decoration: none;
-    }
+  }
+  &:visited {
+    color: #fff;
+    text-decoration: none;
+  }
 `;
 const SidebarLabel = styled.span`
-    margin-left: 1rem;
+  margin-left: 1rem;
 `;
 const DropdownLink = styled(Link)`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    height: 3.75rem;
-    font-size: 15px;
-    padding-left: 5rem;
-    text-decoration: none;
-    color: #ffffff;
-    &:hover {
-        background-color: #9c27b0;
-        color:#fff;
-    }
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 3.75rem;
+  font-size: 15px;
+  padding-left: 5rem;
+  text-decoration: none;
+  color: #ffffff;
+  &:hover {
+    background-color: #9c27b0;
+    color: #fff;
+  }
 `;
 const DropdownLinkChild = styled(Link)`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    height: 3.75rem;
-    font-size: 15px;
-    padding-left: 7rem;
-    text-decoration: none;
-    color: #ffffff;
-    &:hover {
-        background-color: #9c27b0;
-        color:#fff;
-    }
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 3.75rem;
+  font-size: 15px;
+  padding-left: 7rem;
+  text-decoration: none;
+  color: #ffffff;
+  &:hover {
+    background-color: #9c27b0;
+    color: #fff;
+  }
 `;
 
-const Submenu = ({ item }: any,path:string) => {
-
-  const [pathvalue , setPathvalue]= useState("");
-const location= useLocation();
+const Submenu = ({ item }: any, path: string) => {
+  const [pathvalue, setPathvalue] = useState("");
+  const location = useLocation();
   const [subNav, setsubNav] = useState(false);
   const [subMenu, setsubMenu] = useState(false);
   const showsubNav = () => setsubNav(!subNav);
   const showsubNavMenu = () => setsubMenu(!subMenu);
- 
-  useEffect( () => {
+
+  useEffect(() => {
     setPathvalue(window.location.pathname);
-  },[location]);
+  }, [location]);
   return (
     <>
       <SidebarLink
         to={item.path}
-        style={pathvalue == item.path ? {backgroundColor:"grey"}:{}}
-        onClick={()=>{setsubNav(!subNav)}}
+        style={pathvalue == item.path ? { backgroundColor: "grey" } : {}}
+        onClick={() => {
+          setsubNav(!subNav);
+        }}
       >
         <div>
           {item.icon}
@@ -87,12 +88,15 @@ const location= useLocation();
             : null}
         </div>
       </SidebarLink>
-      {subNav &&
-        item.subNav?.map((item: any , index:number) => {
+      {subNav && item?.subNav?.map((item: any, index: number) => {
           return (
             <>
               <DropdownLink
-                to={item.path} style={pathvalue == item.path ? {backgroundColor:"grey"}:{}} key = {index}
+                to={item.path}
+                style={
+                  pathvalue == item.path ? { backgroundColor: "grey" } : {}
+                }
+                key={index}
                 onClick={item.subNavMenus && showsubNavMenu}
               >
                 {item.icon}
@@ -110,12 +114,18 @@ const location= useLocation();
                 item.subNavMenus?.map((item: any) => {
                   return (
                     <>
-                    <DropdownLinkChild to={item.path} style={pathvalue == item.path ? {backgroundColor:"grey"}:{}}>
-                     
-                      {item.icon}
-                      <SidebarLabel>{item.title}</SidebarLabel>
-                      {item.iconClosed}
-                    </DropdownLinkChild>
+                      <DropdownLinkChild
+                        to={item.path}
+                        style={
+                          pathvalue == item.path
+                            ? { backgroundColor: "grey" }
+                            : {}
+                        }
+                      >
+                        {item.icon}
+                        <SidebarLabel>{item.title}</SidebarLabel>
+                        {item.iconClosed}
+                      </DropdownLinkChild>
                     </>
                   );
                 })}
@@ -125,8 +135,5 @@ const location= useLocation();
     </>
   );
 };
-
-
-
 
 export default Submenu;
